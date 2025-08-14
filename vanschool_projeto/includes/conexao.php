5da87182920f4381bb116cb4a24041d7cbd4b424
Iasmin Tavares <rebecapelogia8@gmail.com>
2025-08-14 16:15:48 -0300

    Editando BackEnd do cadastro do aluno!

--- vanschool_projeto/includes/conexao.php
+++ vanschool_projeto/includes/conexao.php
@@ -1,12 +1,15 @@
 <?php
-$host = 'localhost';
-$db = 'u451416913_g15';
-$user = 'u451416913_g15';
-$pass = 'Gp15@123';
+// Variáveis de conexão
+$servidor = 'localhost';
+$banco = 'biblioteca_iasminr';
+$usuario = 'root';
+$senha = '';
 
+// Criando a conexão. PDO funciona com qualquer SGBD
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
+    $pdo = new PDO("mysql:host=$servidor;dbname=$banco;charset=utf8", $usuario, $senha);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    die("Erro na conexão: " . $e->getMessage());
+    die('Erro na conexão: ' . $e->getMessage());
 }
 ?>
